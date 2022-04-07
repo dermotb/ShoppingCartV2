e3e0804c1d90b503f79b9a31fd9c846ca3afec3c
Dermot Boyle <dermotb@hotmail.com>
2022-04-07 19:37:08 +0100

    Finished the lab requirements

--- ShoppingCart/Controllers/ShopController.cs
+++ ShoppingCart/Controllers/ShopController.cs
@@ -15,10 +15,39 @@ namespace ShoppingCart.Controllers
 
         
         private static Cart cart = new Cart();
+        private static int instanceCount = 0;
+
+        public ShopController()
+        {
+            instanceCount++;
+        }
 
         public IActionResult Index()
         {
+            ViewBag.TotalPrice = cart.CalculateTotalPrice();
+            ViewBag.LastAdded = instanceCount;
             return View(items);
+        }
+
+        public ActionResult AddToCart(string code)
+        {
+            try
+            {
+                foreach(Item itm in items)
+                {
+                    if (itm.Code == code)
+                    {
+                        cart.AddItem(itm);
+                        break;
+                    }
+                }
+                
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
